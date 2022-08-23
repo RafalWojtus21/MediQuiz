46731815e40ff1754cc2503aa1b15117a1cc3ba3
Rafal Wojtus <rafalwojtus21@gmail.com>
2022-08-23 12:09:28 +0200

    Initial Commit

---
+++ MediQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MediQuiz
+//
+//  Created by Gość on 23/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
